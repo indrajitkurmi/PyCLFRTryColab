c70f23baf5ff731a73fb174511d09e76a78792a8
indrajitkurmi <indrajitkurmi@gmail.com>
2021-03-02 11:54:29 +0100

    Update TestRendererBind_setup_Indrajit.py

--- glesLFR/src/TestRendererBind_setup_Indrajit.py
+++ glesLFR/src/TestRendererBind_setup_Indrajit.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
 from Cython.Distutils import build_ext
-
+import numpy
 #ext = Extension('glesLFR', sources=["glesLFRPyth.pyx","src\glesLFR.cpp","src\LFGenerator.cpp","src\stb_image.cpp","src\glad.c"], language="c++",)
 
 #setup(name="glesLFR", ext_modules = cythonize([ext]),cmdclass = {'build_ext': build_ext})
@@ -15,6 +15,7 @@ setup(
               sources=["glesLFR_Indrajit_Pyth.pyx","glad.c","stb_image.cpp"],
               libraries=["glfw3","assimp",],
               language="c++",
+              include_dirs=[numpy.get_include()]),
               extra_compile_args=["-Wall","-Wextra", "-std=c++17", "-ggdb", "-lpthread","-I../include/","-L../lib/","-llibassimp.so.5"],
               extra_link_args=["-L../lib/"]
               )
